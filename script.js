42e3e79bc59f45f85113fed1a2ef2cc39f30ef56
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2021-08-17 16:28:54 +0200

    FilterFunction

--- script.js
+++ script.js
@@ -20,7 +20,30 @@ function displayUsers(data) {
   data.forEach((user) => {
     let userItem = document.createElement("li");
     userItem.classList.add("list-group-item", "user-item");
-    userItem.innerText = user.name;
+    userItem.innerText = `${user.name}----${user.username}----${user.email}`;
     userList.append(userItem);
+  });
+}
+
+async function filterFunction(data) {
+  let searchField = document.querySelector(".searchField");
+  searchField.addEventListener("change", function (event) {
+    console.log(event.target.value);
+  });
+
+  let filteredUser = document.querySelector(".navigationFilter");
+  let filterOption = document.querySelectorAll("option");
+  filteredUser.addEventListener("change", function (e) {
+    console.log(e.target.value);
+
+    if (e.target.value == "Name") {
+      console.log("this is the name");
+    }
+
+    if (e.target.value == "Username") {
+    }
+
+    if (e.target.value == "E-mail") {
+    }
   });
 }
