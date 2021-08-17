a0d7034f500c2a3e5ededddfcd92e8c9d040adcb
js25s <petrauskas.justas@gmail.com>
2021-08-17 15:12:36 +0200

    1ex done

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 window.onload = async () => {
-    try {
-      const response = await fetch(
-        `https://jsonplaceholder.typicode.com/users`
-      );
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-      }
-    } catch (error) {
-      console.log(error);
+  fetchUsers("https://jsonplaceholder.typicode.com/users");
+};
+async function fetchUsers(url) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayUsers(data);
     }
-  };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayUsers(data) {
+  let userList = document.querySelector(".user-list");
+
+  data.forEach((user) => {
+    let userItem = document.createElement("li");
+    userItem.classList.add("list-group-item", "user-item");
+    userItem.innerText = user.name;
+    userList.append(userItem);
+  });
+}
